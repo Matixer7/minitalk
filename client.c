f870356b81fc29400056b34184b2f2d8e676affd
Linux User <frog@f11501.mikr.us>
2025-11-07 20:36:45 +0100

    Fix: handled multiple words message | TODO: test everything

--- client.c
+++ client.c
@@ -26,9 +26,9 @@ void	send_signal (char *binary, pid_t server_pid )
 	len = ft_strlen(binary);
 	while (i < len)
 	{
-		if (binary[i] == 1)
+		if (binary[i] == '1')
 			kill(server_pid, SIGUSR1);
-		if (binary[i] == 0)
+		if (binary[i] == '0')
 			kill(server_pid, SIGUSR2);
 		usleep(50);
 		i++;
@@ -43,7 +43,7 @@ void	ascii_to_binary (int c, pid_t server_pid)
 	i = 0;
 	while (c != 0)
 	{
-		binary[i] = c % 2;
+		binary[i] = c % 2 + '0';
 		c = c / 2;
 		i++;
 	}
@@ -55,17 +55,22 @@ int main(int argc, char **argv)
 {
 	pid_t server_pid;
 	int i;
+	int j;
 	
 	if (!argv[1] || !ft_atoi(argv[1]))
 		return (ft_printf("Wrong PID format or no PID given"));
 	if (!argv[2])
 		return (ft_printf("Nothing to send :c"));
 	i = 0;
-	while (argv[2][i])
+	j = 0;
+	while(argv[j])
 	{
-		ascii_to_binary(argv[2][i], server_pid);
-		i++;
+		while (argv[j][i])
+		{
+			ascii_to_binary(argv[j][i], server_pid);
+			i++;
+		}
+		j++;
 	}
-	
 	return (0);
 }
