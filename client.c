809c5479199be2e82236ac83c35f74196d3508be
Linux User <frog@f11501.mikr.us>
2025-11-07 19:23:19 +0100

    Feat: handled converting messages into binary and sending them | to be tested

--- client.c
+++ client.c
@@ -17,34 +17,54 @@
 #include <signal.h>
 #include <stdlib.h>
 
+void	send_signal (char *binary, pid_t server_pid )
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = ft_strlen(binary);
+	while (i < len)
+	{
+		if (binary == 1)
+			kill(server_pid, SIGUSR1);
+		if (binary == 0)
+			kill(server_pid, SIGUSR2);
+		usleep(50);
+	}
+}
+
+void	ascii_to_binary (int c, pid_t server_pid)
+{
+	char binary[9];
+	int i;
+	
+	i = 0;
+	while (c != 0)
+	{
+		binary[i] = c % 2;
+		c = c / 2;
+		i++;
+	}
+	binary[8] = '\0';
+	send_signal(binary)
+}
+
 int main(int argc, char **argv)
 {
 	pid_t server_pid;
+	int i;
 	
 	if (!argv[1] || !ft_atoi(argv[1]))
 		return (ft_printf("Wrong PID format or no PID given"));
-		
-	int i = 0;
-	while(i <= 20)
+	if (!argv[2])
+		return (ft_printf("Nothing to send :c");
+	i = 0;
+	while (argv[2][i])
 	{
-		server_pid = ft_atoi(argv[1]);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR1);
-		usleep(100);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR2);
-		usleep(100);
-		kill(server_pid, SIGUSR1);
-		usleep(100);
+		ascii_to_binary(argv[2][i], server_pid);
 		i++;
 	}
-	return 0;
+	
+	return (0);
 }
